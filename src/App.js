7bbca34c2e098015df11a56ea21e2ec17d01b6fd
Ponmile Adebisi <ponmileadebisi@gmail.com>
2022-07-22 15:49:37 +0100

    DisplayImage component and styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/header/Header';
+import DisplayImage from './components/display/DisplayImage';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <DisplayImage />
     </div>
   );
 }
---
+++ src/components/display/DisplayImage.js
@@ -0,0 +1,50 @@
+import "./DisplayImage.css";
+
+const DisplayImage = () => {
+  return (
+    <div className="display-section">
+      <div className="image-section">
+        <div className="image-container">
+          <div className="image-control">
+            <img
+              src="https://images.unsplash.com/photo-1579260668779-fb03c244ab66?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fG11c2ljfGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+              alt="piano"
+              className="image"
+            />
+            <p>Title</p>
+            <p>Description</p>
+          </div>
+          <div className="image-control">
+            <img
+              src="https://images.unsplash.com/photo-1579260668779-fb03c244ab66?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fG11c2ljfGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+              alt="piano"
+              className="image"
+            />
+            <p>Title</p>
+            <p>Description</p>
+          </div>
+          <div className="image-control">
+            <img
+              src="https://images.unsplash.com/photo-1579260668779-fb03c244ab66?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fG11c2ljfGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+              alt="piano"
+              className="image"
+            />
+            <p>Title</p>
+            <p>Description</p>
+          </div>
+          <div className="image-control">
+            <img
+              src="https://images.unsplash.com/photo-1579260668779-fb03c244ab66?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fG11c2ljfGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+              alt="piano"
+              className="image"
+            />
+            <p>Title</p>
+            <p>Description</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DisplayImage;
